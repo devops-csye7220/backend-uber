0878dc587fbe21351c7df7ff3465d0b283a1317f
Dileep Kumar Reddy Sirasani <sirasani.d@husky.neu.edu>
2021-04-25 11:30:44 -0400

    added stage for endpoint testing

--- tests/user-model.test.js
+++ tests/user-model.test.js
@@ -1,50 +1,50 @@
-/* User model tests */
-const UserModel = require('../api/models/user');
-const mongoose = require('mongoose');
-const userData = { _id: new mongoose.Types.ObjectId(), firstname: "Jonah", lastname: "Smith", email: "jonah1234@gmail.com", password: "foobar", usertype: "customer" };
+// /* User model tests */
+// const UserModel = require('../api/models/user');
+// const mongoose = require('mongoose');
+// const userData = { _id: new mongoose.Types.ObjectId(), firstname: "Jonah", lastname: "Smith", email: "jonah1234@gmail.com", password: "foobar", usertype: "customer" };
 
-const test_uri = "mongodb+srv://sdileepkumarreddy:Qwerty123@cluster0.j477k.mongodb.net/pp-test";
+// const test_uri = "mongodb+srv://sdileepkumarreddy:Qwerty123@cluster0.j477k.mongodb.net/pp-test";
 
-describe('insert new user into collection', () => {
-  beforeAll(async () => {
-    await mongoose.connect(test_uri, { useNewUrlParser: true, useCreateIndex: true }, (err) => {
-      if (err) {
-        console.error(err);
-        process.exit(1);
-      }
-    });
-  });
+// describe('insert new user into collection', () => {
+//   beforeAll(async () => {
+//     await mongoose.connect(test_uri, { useNewUrlParser: true, useCreateIndex: true }, (err) => {
+//       if (err) {
+//         console.error(err);
+//         process.exit(1);
+//       }
+//     });
+//   });
 
-  afterAll(async () => {
-    mongoose.connection.db.dropCollection("users", function (err, result) { });
-  });
+//   afterAll(async () => {
+//     mongoose.connection.db.dropCollection("users", function (err, result) { });
+//   });
 
-  it('create and save user successfully', async () => {
-    const validUser = new UserModel(userData);
-    const savedUser = await validUser.save();
+//   it('create and save user successfully', async () => {
+//     const validUser = new UserModel(userData);
+//     const savedUser = await validUser.save();
 
-    expect(savedUser._id).toBeDefined();
-    expect(savedUser.firstname).toBe(userData.firstname);
-    expect(savedUser.lastname).toBe(userData.lastname);
-    expect(savedUser.email).toBe(userData.email);
-    expect(savedUser.userType).toBe(userData.userType);
-  });
+//     expect(savedUser._id).toBeDefined();
+//     expect(savedUser.firstname).toBe(userData.firstname);
+//     expect(savedUser.lastname).toBe(userData.lastname);
+//     expect(savedUser.email).toBe(userData.email);
+//     expect(savedUser.userType).toBe(userData.userType);
+//   });
 
-  it('create user without required field should fail', async () => {
-    const invalidUserData = { _id: new mongoose.Types.ObjectId(), lastname: "Smith", email: "jonah1234@gmail.com", password: "foobar", usertype: "customer" };
+//   it('create user without required field should fail', async () => {
+//     const invalidUserData = { _id: new mongoose.Types.ObjectId(), lastname: "Smith", email: "jonah1234@gmail.com", password: "foobar", usertype: "customer" };
 
-    let err;
+//     let err;
 
-    try {
-      const invalidUser = new UserModel(invalidUserData);
-      const savedUserWithoutFirstname = await invalidUser.save();
-      error = savedUserWithoutFirstname;
-      console.log(error);
-    } catch (error) {
-      err = error
-    }
+//     try {
+//       const invalidUser = new UserModel(invalidUserData);
+//       const savedUserWithoutFirstname = await invalidUser.save();
+//       error = savedUserWithoutFirstname;
+//       console.log(error);
+//     } catch (error) {
+//       err = error
+//     }
 
-    expect(err).toBeInstanceOf(mongoose.Error.ValidationError);
-    expect(err.errors.firstname).toBeDefined();
-  });
-});
+//     expect(err).toBeInstanceOf(mongoose.Error.ValidationError);
+//     expect(err.errors.firstname).toBeDefined();
+//   });
+// });
